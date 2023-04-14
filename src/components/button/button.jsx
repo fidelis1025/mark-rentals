e87884c16b9f8b24d2a625bebe9c8fcdd6e991b5
Fidelis <mac@Fidelis-2.local>
2023-04-14 14:45:15 +0100

    added useNavigate to move to another page on button click

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React from "react";
 
-const Button = ({className, children}) => {
+const Button = ({ className, handleClick, children }) => {
   return (
     <div>
-        <button className={`md:h-[3rem] md:text-base md:font-normal md:rounded-xl md:bg-[#F4511E] hover:border-2 hover:bg-transparent hover:text-[#252B42] hover:border-[#F4511E] ${className}`} type='button'>
-            {children}
-        </button>
+      <button
+        onClick={handleClick}
+        className={`md:h-[3rem] md:text-base md:font-normal md:rounded-xl md:bg-[#F4511E] hover:border-2 hover:bg-transparent hover:text-[#252B42] hover:border-[#F4511E] ${className}`}
+        type="button"
+      >
+        {children}
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Button
+
+
+export default Button;
--- src/components/property-heading/property-heading.jsx
+++ src/components/property-heading/property-heading.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import Button from "../button/button";
 
+import { useNavigate } from 'react-router-dom';
+
+
 const PropertyHeading = () => {
+  const navigate = useNavigate();
+
+
+const handleClick =()=> {
+     navigate("/properties");
+  }
+
   return (
-    <div className="md:flex justify-between font-Inter md:px-32 md:py-12 md:bg-[#F5F5F5] ">
+    <div className="md:flex md:justify-between md:font-Inter md:px-32 md:py-12 md:bg-[#F5F5F5] ">
       <h3 className="capitalize md:text-3xl md:font-extrabold md:text-[#000000]">
         List of Properties
       </h3>
-      <Button className="property">View All Properties</Button>
+      <Button handleClick={handleClick} className="property">
+        View All Properties
+      </Button>
     </div>
   );
 };
