81173ebad7a4d0ac1c751f124e5d5aa8c03a914c
Fidelis <undiefidelis@gmail.com>
2023-04-25 12:47:33 +0100

    added header and footer to contact page

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Navbar from '../components/navbar'
+import Footer from '../components/footer'
 
 const Contact = () => {
+  const property = "md:bg-[#545454ce]"
   return (
     <div>
-        <Navbar/>
+        <Navbar property={property}/>
+        <Footer/>
     </div>
   )
 }
